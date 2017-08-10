4762c72b39c15398d5ea82ca5e487f7c28b4a6c4
ckhui <ckhanghui@gmail.com>
2017-08-10 15:21:27 +0800

    Initial Commit

---
+++ W2-D5 Autolayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  W2-D5 Autolayout
+//
+//  Created by CKH on 10/08/2017.
+//  Copyright © 2017 CKH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
